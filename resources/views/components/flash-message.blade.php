8a80f5675c6ec15fdd1d982bf201531964815297
Victor <victor.abbah@imometrics.com>
2023-12-23 10:32:06 +0100

    refactored alert-dismiss icon

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -5,9 +5,13 @@
       <div>
         <p class="font-bold">Success!</p>
         <p class="text-sm">{{session('success')}}</p>
-        <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
-            <svg @click="show = false" class="fill-current h-6 w-6 text-grey-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
-        </span>
+        <button x-on:click="show = false" class="!absolute top-2 right-2 h-6 max-h-[24px] w-6 max-w-[24px] select-none rounded-lg text-center align-middle font-sans text-xs font-medium uppercase text-grey transition-all hover:bg-black/10 active:bg-black/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button">
+            <span class="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-5 h-5" stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"></path>
+                </svg>
+            </span>
+        </button>
       </div>
     </div>
   </div>
