0a45b182b122e28fc313fc86bc5c849b80a6cc97
Victor <victor.abbah@imometrics.com>
2024-01-05 22:11:29 +0100

    created listener to send email

---
+++ app/Listeners/SendVerificationMail.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class SendVerificationMail
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(object $event): void
+    {
+        // send verification mail when new user is created
+        Mail::to($event->user)->send(new \App\Mail\VerificationMail($event->user));
+    }
+}
