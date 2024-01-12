48385abf9134a284edf7cd97871a78e178a4430e
Victor <victor.abbah@imometrics.com>
2024-01-12 19:32:14 +0100

    welcome email listener

---
+++ app/Listeners/SendWelcomeMail.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class SendWelcomeMail
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(object $event): void
+    {
+        // send verification mail when new user is created
+        Mail::to($event->user)->queue(new \App\Mail\WelcomeMail($event->user));
+    }
+}
