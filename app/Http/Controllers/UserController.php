685f8a9fcb8153c12d87513ff33595f7a2578743
Victor <victor.abbah@imometrics.com>
2024-01-16 15:27:35 +0100

    adjusted alert in forgotpassword function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -130,7 +130,7 @@ class UserController extends Controller
         );
 
         return $status === Password::RESET_LINK_SENT
-            ? back()->with(['status' => __($status)])
+            ? back()->with(['success' => __($status)])
             : back()->withErrors(['email' => __($status)]);
     }
 
