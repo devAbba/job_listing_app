5fc962da2635591255e0198405ad22cafe8553b6
Victor <victor.abbah@imometrics.com>
2024-02-05 08:44:56 +0100

    acl policies

---
+++ app/Policies/ListingPolicy.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Listing;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class ListingPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Listing $listing): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Listing $listing): bool
+    {
+        return $listing->user_id === $user->id;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function destroy(User $user, Listing $listing): bool
+    {
+        return ($user->is_admin || $listing->user_id === $user->id);
+    }
+
+}
